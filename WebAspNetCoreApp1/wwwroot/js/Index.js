20b3d5a1afc53760049333382ab16a617dd8394c
Eduardo Ferreira <eduferreira1989@gmail.com>
2018-07-23 22:28:38 -0300

    Create.

---
+++ WebAspNetCoreApp1/wwwroot/js/Index.js
@@ -0,0 +1,21 @@
+﻿//Só executa o conteúdo quando o documento estiver carregado
+$(document).ready(function () {
+    var $meuButton = $("#meu-button");
+    $meuButton.on("click", function () {
+        alert("Teste Evento Função Anônima");
+    });
+
+    var $precos = $(".preco-produto li");
+    $precos.on("click", function () {
+        alert($(this).text());
+    });
+
+    var $login = $("#login");
+    var $popupForm = $(".popup-form");
+    $login.on("click", function () {
+        $popupForm.toggle(1000);
+    });
+});
+
+
+
